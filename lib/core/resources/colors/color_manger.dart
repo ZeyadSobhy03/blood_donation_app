7814dd17ef4cad0a65bea0ce68fa19ee9aa164ee
ZiyadSobhy <ziad60189@gmail.com>
2026-02-02 01:06:23 +0200

    create file color manger

---
+++ lib/core/resources/colors/color_manger.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+class ColorManger {
+  static const Color brightRed = Color(0xFFFF3B3B);
+  static const Color softPinkishWhite = Color(0xFFFFF5F5);
+  static const Color pureWhite = Color(0xFFFFFFFF);
+  static const Color lightBlueGrey = Color(0xFFE2E8F0);
+  static const Color mutedGrey = Color(0xFFCBD5E1);
+  static const Color darkSlate = Color(0xFF1E293B);
+  static const Color slateGrey = Color(0xFF64748B);
+  static const Color slateBlueGrey = Color(0xFF64748B);
+  static const Color lightGrey = Color(0xFFE2E8F0);
+  static const Color mutedGreyIndicator = Color(0xFFCBD5E1);
+
+  static const Color softBlueWhite = Color(0xFFF0F7FF);
+  static const Color royalBlue = Color(0xFF2563EB);
+  static const Color skyBlue = Color(0xFF3B82F6);
+  static const Color lightGreyBorder = Color(0xFFE2E8F0);
+}
