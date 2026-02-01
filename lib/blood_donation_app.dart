56f2e8c7686771236b91ca514c9db37da91b0b1e
ZiyadSobhy <ziad60189@gmail.com>
2026-02-02 01:23:46 +0200

    install screenutil plugin

--- lib/blood_donation_app.dart
+++ lib/blood_donation_app.dart
@@ -1,15 +1,21 @@
 import 'package:blood_donation_app/core/resources/routes/route_manger.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BloodDonationApp extends StatelessWidget {
   const BloodDonationApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: RouteManger.router,
-      initialRoute: RouteManger.onboarding,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: RouteManger.router,
+        initialRoute: RouteManger.onboarding,
+      ),
     );
   }
 }
--- lib/presentation/onboarding/onboarding_page.dart
+++ lib/presentation/onboarding/onboarding_page.dart
@@ -1,10 +1,27 @@
+import 'package:blood_donation_app/core/resources/colors/color_manger.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: ColorManger.softPinkishWhite,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Icon(
+              Icons.favorite_border,
+              color: ColorManger.brightRed,
+              size: 100.sp,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
