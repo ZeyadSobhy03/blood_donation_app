77cc182c40d8504682e77158bcc50845fc662982
ZiyadSobhy <ziad60189@gmail.com>
2026-02-02 01:13:34 +0200

    refactor Route manger

--- lib/core/resources/routes/route_manger.dart
+++ lib/core/resources/routes/route_manger.dart
@@ -1,3 +1,6 @@
+import 'package:blood_donation_app/core/resources/colors/color_manger.dart';
+import 'package:blood_donation_app/core/resources/fonts/font_manger.dart';
+import 'package:blood_donation_app/core/widgets/custom_text.dart';
 import 'package:blood_donation_app/presentation/onboarding/onboarding_page.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +14,18 @@ class RouteManger {
       default:
         return MaterialPageRoute(
           builder: (context) {
-            return Scaffold(body: Center(child: Text('page not found')));
+            return Scaffold(
+              body: Center(
+                child: CustomText(
+                  text: 'page not found',
+                  textStyle: TextStyle(
+                    color: ColorManger.brightRed,
+                    fontSize: FontSize.s24,
+                    fontWeight: FontWeightManager.bold,
+                  ),
+                ),
+              ),
+            );
           },
         );
     }
