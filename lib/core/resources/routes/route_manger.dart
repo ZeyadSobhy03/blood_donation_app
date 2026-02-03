002da0bdd4ec0ba6a55eacd8fdb7fd6b5b2446aa
ZiyadSobhy <ziad60189@gmail.com>
2026-02-03 15:32:08 +0200

    create folder and file of donor and routes

--- lib/core/resources/routes/route_manger.dart
+++ lib/core/resources/routes/route_manger.dart
@@ -3,11 +3,13 @@ import 'package:blood_donation_app/core/resources/fonts/font_manger.dart';
 import 'package:blood_donation_app/core/widgets/custom_text.dart';
 import 'package:blood_donation_app/presentation/choose_role/choose_role.dart';
 import 'package:blood_donation_app/presentation/onboarding/onboarding_pages.dart';
+import 'package:blood_donation_app/presentation/role/donor/tabs/main_layout.dart';
 import 'package:flutter/material.dart';
 
 class RouteManger {
   static const String onboarding = '/onboarding';
   static const String chooseRole = '/chooseRole';
+  static const String donorMainLayout = '/donorMainLayout';
 
   static Route router(RouteSettings settings) {
     switch (settings.name) {
@@ -16,6 +18,9 @@ class RouteManger {
 
       case chooseRole:
         return MaterialPageRoute(builder: (context) => ChooseRole());
+
+      case donorMainLayout:
+        return MaterialPageRoute(builder: (context) => MainLayout());
       default:
         return MaterialPageRoute(
           builder: (context) {
