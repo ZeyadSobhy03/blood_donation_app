ac06ccb7041d84b838aed788cc67bd89be3126da
ZiyadSobhy <ziad60189@gmail.com>
2026-04-20 23:30:22 +0200

    edit in admin request tab and edit in blood request model

--- lib/core/resources/models/blood_request.dart
+++ lib/core/resources/models/blood_request.dart
@@ -7,6 +7,12 @@ class BloodRequestModel {
   final bool isFulfilled;
   final DateTime requestDate;
   final int completionTimeInHours;
+  final RequestPriority priority;
+  final String? id;
+  final String location;
+  final String hospitalContact;
+  final String hospitalName;
+
 
   BloodRequestModel({
     required this.bloodType,
@@ -17,5 +23,11 @@ class BloodRequestModel {
     required this.isFulfilled,
     required this.requestDate,
     required this.completionTimeInHours,
+    required this.priority, this.id, required this.location, required this.hospitalContact, required this.hospitalName,
   });
+}
+enum RequestPriority {
+  low,
+  medium,
+  high,
 }
